f1b2763cd164f9c832ae3184239ddde56f76ebf6
Camille Sadoff <csadoff00@stuy.edu>
2018-11-05 23:15:20 -0500

    wrote rowSum

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,3 +1,12 @@
 public class ArrayMethods {
-  
+  public static int rowSum(int[][] ary, int x) {
+    int sum=0;
+    for (int i1=0; i1<ary.length; i1++) {
+      for (int i2=0; i2<ary[i1].length; i2++) {
+        sum += ary[i1][i2];
+      }
+    }
+    return sum;
+  }
+
 }
