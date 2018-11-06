53ceb45f7ada107e249696bd4c72134f4232091b
Camille Sadoff <csadoff00@stuy.edu>
2018-11-05 23:22:51 -0500

    wrote columnSum

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -9,4 +9,15 @@ public class ArrayMethods {
     return sum;
   }
 
+  public static int columnSum(int[][] ary, int x) {
+    int sum=0;
+    int i1=0;
+    for (int i2=0; i2<ary[i1].length; i2++) {
+      for (i1=0; i1<ary.length; i1++) {
+        sum += ary[i1][i2];
+      }
+    }
+    return sum;
+  }
+
 }
